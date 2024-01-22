a66ef27cc74b736c1bd91311daf06907b4dd8cb0
Vyacheslav <social_smm@bk.ru>
2024-01-23 00:49:58 +0300

    fix

--- Library/Book.cs
+++ Library/Book.cs
@@ -3,97 +3,95 @@
 namespace ClassLibrary2
 {
     public class Book
-{
-    // Поля
-
-    // Название книги
-    private string title;
-
-    // Автор книги
-    private string author;
-
-    // Код издания книги
-    private int editionCode;
-
-    // Текстовое описание книги
-    private string description;
-
-    // Статус книги (в библиотеке, забронирована, на руках)
-    private string status;
-
-    // Конструкторы
-
-    // Конструктор для создания новой книги
-    public Book(string title, string author, int editionCode, string description)
     {
-        this.title = title;
-        this.author = author;
-        this.editionCode = editionCode;
-        this.description = description;
-        this.status = "в библиотеке"; // По умолчанию книга находится в библиотеке
-    }
+        // Поля
 
-    // Методы
+        // Название книги
+        private string title;
 
-    // Метод для изменения статуса книги на "забронирована"
-    public void ReserveBook()
-    {
-        if (status == "в библиотеке")
+        // Автор книги
+        private string author;
+
+        // Код издания книги
+        private int editionCode;
+
+        // Текстовое описание книги
+        private string description;
+
+        // Статус книги (в библиотеке, забронирована, на руках)
+        private string status;
+
+        // Конструкторы
+
+        // Конструктор для создания новой книги
+        public Book(string title, string author, int editionCode, string description)
         {
-            status = "забронирована";
-            Console.WriteLine($"Книга '{title}' забронирована.");
+            this.title = title;
+            this.author = author;
+            this.editionCode = editionCode;
+            this.description = description;
+            this.status = "в библиотеке"; // По умолчанию книга находится в библиотеке
         }
-        else
-        {
-            Console.WriteLine("Книгу нельзя забронировать в данный момент.");
-        }
-    }
 
-    // Метод для изменения статуса книги на "на руках"
-    public void BorrowBook()
-    {
-        if (status == "в библиотеке" || status == "забронирована")
-        {
-            status = "на руках";
-            Console.WriteLine($"Книга '{title}' взята на руки.");
-        }
-        else
-        {
-            Console.WriteLine("Книгу нельзя взять в данный момент.");
-        }
-    }
+        // Методы
 
-    // Метод для изменения статуса книги на "в библиотеке"
-    public void ReturnBook()
-    {
-        if (status == "на руках")
+        // Метод для изменения статуса книги на "забронирована"
+        public void ReserveBook()
         {
-            status = "в библиотеке";
-            Console.WriteLine($"Книга '{title}' возвращена в библиотеку.");
+            if (status == "в библиотеке")
+            {
+                status = "забронирована";
+                Console.WriteLine($"Книга '{title}' забронирована.");
+            }
+            else
+            {
+                Console.WriteLine("Книгу нельзя забронировать в данный момент.");
+            }
         }
-        else
+
+        // Метод для изменения статуса книги на "на руках"
+        public void BorrowBook()
         {
-            Console.WriteLine("Книгу нельзя вернуть в данный момент.");
+            if (status == "в библиотеке" || status == "забронирована")
+            {
+                status = "на руках";
+                Console.WriteLine($"Книга '{title}' взята на руки.");
+            }
+            else
+            {
+                Console.WriteLine("Книгу нельзя взять в данный момент.");
+            }
+        }
+
+        // Метод для изменения статуса книги на "в библиотеке"
+        public void ReturnBook()
+        {
+            if (status == "на руках")
+            {
+                status = "в библиотеке";
+                Console.WriteLine($"Книга '{title}' возвращена в библиотеку.");
+            }
+            else
+            {
+                Console.WriteLine("Книгу нельзя вернуть в данный момент.");
+            }
+        }
+
+        // Геттер для получения статуса книги
+        public string Status
+        {
+            get { return status; }
+        }
+
+        // Геттер для получения названия книги
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public string Author
+        {
+            get { return author; }
         }
     }
-
-    // Геттер для получения статуса книги
-    public string Status
-    {
-        get { return status; }
-    }
-
-    // Геттер для получения названия книги
-    public string Title
-    {
-        get { return title; }
-    }
-    
-    public string Author
-    {
-        get { return author; }
-    }
-    
-}
-
 }
